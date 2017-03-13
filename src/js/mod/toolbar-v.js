0af21b42a7652d174671102d6514e2cc8e3bb628
Mark <mkimmet@gmail.com>
2017-03-12 23:25:41 -0400

    Update toolbar-v.js

--- src/js/mod/toolbar-v.js
+++ src/js/mod/toolbar-v.js
@@ -71,12 +71,12 @@ function( $, _, Backbone, gridModel, gridController, windowView ) {
 		
 		onNodeLoad: function(evt) {
 			var str = this.$('.node-url').val();
-		    var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-		    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-		    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-		    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-		    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-		    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+		    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 		  if(!pattern.test(str)) {
 		    gridModel.reset(JSON.parse(str));
 		  } else {
