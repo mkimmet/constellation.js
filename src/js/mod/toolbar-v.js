cd84cf6a5a91f58a77fbb5391c7553c547a58a91
Mark <mkimmet@gmail.com>
2017-03-12 22:09:21 -0400

    Update toolbar-v.js

--- src/js/mod/toolbar-v.js
+++ src/js/mod/toolbar-v.js
@@ -69,7 +69,7 @@ function( $, _, Backbone, gridModel, gridController, windowView ) {
 		},
 		
 		onNodeLoad: function(evt) {
-			alert("YO!");
+			alert(this.$('.node-url').val());
 			evt.preventDefault();
 		},
 		
